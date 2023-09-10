f0cb95a2df29244e5e30844cddb25f2c35654008
Gurbaksh <gill.577@buckeyemail.osu.edu>
2023-09-10 18:56:38 -0400

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img className="earth" src={require('./earth.png')} alt="earth" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
